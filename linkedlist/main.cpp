4cfc728701872fa6501fd148066bb3b139bd60fd
erin zhang <erinzhang@erindemacbook-air.local>
2016-11-06 15:34:08 +0800

    Initial Commit

---
+++ linkedlist/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  linkedlist
+//
+//  Created by erin zhang on 6/11/2016.
+//  Copyright © 2016 erin zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
